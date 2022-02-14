e367df8548ac3d8a3b5ae37391ea7675a854a97f
zelek684 <angelikalazoryszyn684@wp.pl>
2022-02-14 10:25:51 +0100

    up

--- 163091-E01/Z3/src/com/company/Main.java
+++ 163091-E01/Z3/src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static File[] pliki(String dir,String ext)
     {
         File tmp = new File(dir);
-        FilenameFilter filter = (filter,s) -> !s.endsWith(ext) &&!file.isDirectory()
-                return tmp.listFiles(filter)
+        FilenameFilter filter = (filter,s) -> !s.endsWith(ext) &&!file.isDirectory();
+                return tmp.listFiles(filter);
     }
     public static void Main(String[]args){
 
